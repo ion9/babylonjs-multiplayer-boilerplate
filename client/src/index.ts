bd08feffe84f8f7266cfad0ea566f9e2123a03c7
ion9 <ion9@users.noreply.github.com>
2019-08-16 21:11:16 -0400

    add ccpwgl2

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,11 @@
 import "./index.css";
 
+import * as CCPWGL2 from "CCPWGL2";
+
+// Other files i think we need
+// <script type="text/javascript" src="../dist/ccpwgl2_int.js"></script>
+// <script type="text/javascript" src="../dist/ccpwgl.js"></script>
+
 import * as BABYLON from "babylonjs";
 import Keycode from "keycode.js";
 
@@ -9,29 +15,47 @@ import { client } from "./game/network";
 // This is optional, but highly recommended
 import { StateHandler } from "../../server/src/rooms/StateHandler";
 import { PressedKeys } from "../../server/src/entities/Player";
+import { Z_NEED_DICT } from "zlib";
 
 const canvas = document.getElementById('game') as HTMLCanvasElement;
+
+// Old
 const engine = new BABYLON.Engine(canvas, true);
+// New
+// ccpwgl.initialize(canvas, demos.options);
 
 // This creates a basic Babylon Scene object (non-mesh)
 var scene = new BABYLON.Scene(engine);
+// scene = ccpwgl.loadScene('res:/dx9/scene/wormholes/wormhole_class_01.red');
 
 // This creates and positions a free camera (non-mesh)
 var camera = new BABYLON.FollowCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-// This targets the camera to scene origin
+// OLD This targets the camera to scene origin
 camera.setTarget(BABYLON.Vector3.Zero());
 
-// This attaches the camera to the canvas
+// OLD This attaches the camera to the canvas
 camera.attachControl(canvas, true);
 
-// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+// NEW 
+// var camera = new ccpwgl.createCamera(canvas);
+//                 camera.minDistance = 0.0;
+//                 camera.maxDistance = 9007199254740990;
+//                 camera.fov = 60;
+//                 camera.distance = 1000000;
+//                 camera.farPlane = 9007199254740990;
+//                 camera.nearPlane = 0.00000001;
+//                 camera.minPitch = -0.5;
+//                 camera.maxPitch = 0.35;
+//                 ccpwgl.setCamera(camera);
+
+// NO NEED This creates a light, aiming 0,1,0 - to the sky (non-mesh)
 var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
 
-// Default intensity is 1. Let's dim the light a small amount
+// NO NEED Default intensity is 1. Let's dim the light a small amount
 light.intensity = 0.7;
 
-// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+// NO MEED Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
 var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 
 // Attach default camera mouse navigation
@@ -40,13 +64,14 @@ var ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 // Colyseus / Join Room
 const room = client.join<StateHandler>("game");
 room.onJoin.add(() => {
-    const playerViews: {[id: string]: BABYLON.Mesh} = {};
+    const playerViews: {[id: string]: BABYLON.Mesh} = {};  // ??????
 
     room.state.players.onAdd = function(player, key) {
         // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
         playerViews[key] = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+        // PlayerViews[key] = scene.loadShip('rdh3_t1:roguebase:rogue');
 
-        // Move the sphere upward 1/2 its height
+        // NO NEED Move the sphere upward 1/2 its height
         playerViews[key].position.set(player.position.x, player.position.y, player.position.z);
 
         // Set camera to follow current player
@@ -61,6 +86,7 @@ room.onJoin.add(() => {
 
     room.state.players.onRemove = function(player, key) {
         scene.removeMesh(playerViews[key]);
+        // scene.removeObject(playerViews[key]);
         delete playerViews[key];
     };
 
